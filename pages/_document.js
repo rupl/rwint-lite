635546866d45e978bff1de3d97db6408fdde6dc5
Amy Benson <avbenson@gmail.com>
2017-08-22 16:59:47 +0100

    Inline the polyfill instead

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,11 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
+const polyfill = () => {
+  // https://cdn.polyfill.io/v2/polyfill.min.js?features=Object.assign|always
+  let data = `(function(undefined) {Object.assign=function(r,t){for(var n,e,o=1;o<arguments.length;++o){e=arguments[o];for(n in e)Object.prototype.hasOwnProperty.call(e,n)&&(r[n]=e[n])}return r};}).call('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});`
+  return {__html: data}
+}
+
 export default class MyDocument extends Document {
   render () {
     const { html } = this.props
@@ -7,7 +13,7 @@ export default class MyDocument extends Document {
     return (
       <html lang='en'>
         <Head>
-          <script src='https://cdn.polyfill.io/v2/polyfill.min.js?features=Object.assign' />
+          <script dangerouslySetInnerHTML={polyfill()} />
         </Head>
         <body>
           <Main />
