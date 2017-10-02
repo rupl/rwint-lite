72e21479a29331aa8d4276725a2316059d262857
Amy Benson <avbenson@gmail.com>
2017-10-02 11:40:22 +0100

    Moved base styles to Layout as not minified when imported

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,7 @@ import Head from 'next/head'
 import Header from './header/Header'
 import Footer from './Footer'
 import NewRelic from './NewRelic'
-import { fontSizes } from '../theme/variables'
-import base from '../theme/base'
+import { breakpoints, colors, fonts, fontSizes, measurements } from '../theme/variables'
 
 const pageTitle = (home, title) => {
   const siteTitle = 'ReliefWeb Mobile'
@@ -36,7 +35,77 @@ const Layout = (props) => (
       </div>
     </div>
     <Footer />
-    <style jsx global>{base}</style>
+    <style jsx global>{`
+      * {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+      }
+      *:before, *:after {
+         box-sizing: border-box;
+      }
+      html, body, h1, h2, h3, h4, h5, h6 {
+        margin: 0;
+        padding: 0;
+      }
+      html {
+        font-size: 100%;
+        background: ${colors.bg.body};
+      }
+      body {
+        font-family: ${fonts.body};
+        font-size: ${fontSizes.base};
+        color: ${colors.text.body};
+        background: ${colors.bg.content};
+        max-width: ${measurements.maxWidth};
+        margin: 0 auto;
+      }
+      .container {
+        padding: 0 ${measurements.baseUnit}em;
+        margin: 0 auto;
+        max-width: ${measurements.containerMaxWidth};
+      }
+      h1 {
+        font-weight: normal;
+        font-family: ${fonts.heading};
+      }
+      h1, h2, h3, h4, h5, h6 {
+        font-size: 100%;
+      }
+      p, ul, ol {
+        line-height: 1.5;
+      }
+      a {
+        color: ${colors.link.default};
+        word-break: break-word;
+      }
+      a:focus {
+        outline: thin dotted;
+      }
+      button {
+        -webkit-appearance: none;
+        font-size: 100%;
+      }
+      .sr-only {
+        position: absolute;
+        width: 1px;
+        height: 1px;
+        padding: 0;
+        margin: -1px;
+        overflow: hidden;
+        clip: rect(0,0,0,0);
+        border: 0;
+      }
+      @media (min-width: ${breakpoints.md}) {
+        html {
+          padding: 0 ${measurements.baseUnit}em;
+        }
+      }
+      @media (min-width: ${breakpoints.xl}) {
+        .container {
+          padding: 0 ${measurements.baseUnit * 5}em;
+        }
+      }
+    `}</style>
     <style jsx>{`
       .skip-link {
         position: absolute;
