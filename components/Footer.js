fce200faf95b046e915d2e45bf5feae1f602c894
Amy Benson <avbenson@gmail.com>
2017-10-02 10:08:25 +0100

    Common design footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,85 @@
+import { breakpoints, colors, fontSizes, measurements } from '../theme/variables'
+
 const Footer = () => (
-  <footer role='contentinfo'>
-    Footer
+  <footer role='contentinfo' className='footer'>
+    <div className='container'>
+      <ul>
+        <li><a href='https://reliefweb.int/'>Full site</a></li>
+        <li><a href='https://reliefweb.int/blog'>Blog</a></li>
+        <li><a href='https://labs.reliefweb.int/'>Labs</a></li>
+        <li><a href='https://reliefweb.int/about'>About us</a></li>
+      </ul>
+
+      <div className='row footer__small-print'>
+        <p>© United Nations Office for the Coordination of Humanitarian Affairs</p>
+      </div>
+    </div>
     <style jsx>{`
       footer {
-        margin-top: 24px;
+        background: ${colors.bg.headerFooter};
+        margin-top: ${measurements.baseUnit * 3}em;
+        padding: ${measurements.baseUnit * 2}em 0;
+        color: ${colors.text.globalHeader};
+      }
+      a {
+        color: ${colors.text.globalHeader};
+        text-decoration: none;
+        font-size: ${fontSizes.small};
+      }
+      a:hover, a:focus {
+        border-bottom: 1px solid rgba(255,255,255,0.6)
+      }
+      ul  {
+        text-transform: uppercase;
+        list-style: none;
+        margin: 0 0 ${measurements.baseUnit * 2}em 0;
+        padding: 0;
+      }
+      li {
+        display:inline-block;
+        position: relative;
+        margin: 0 ${measurements.baseUnit * 2}em 0 0;
+        padding: 0
+      }
+      li:after {
+        content: "|";
+        position: absolute;
+        right: -10px;
+        font-size: ${fontSizes.small};
+        top: 2px;
+        opacity: 0.8;
+      }
+      li:last-child:after {
+        content: none;
+      }
+      p {
+        font-size: ${fontSizes.small};
+        margin-bottom: 0;
+      }
+      @media (min-width: ${breakpoints.md}) {
+        footer {
+          padding: ${measurements.baseUnit * 3}em 0;
+          margin-top: ${measurements.baseUnit * 5}em;
+        }
+        a {
+          font-size: ${fontSizes.base};
+        }
+        li {
+          margin-right: ${measurements.baseUnit * 7}em
+        }
+        li:after {
+          right: -32px;
+          font-size: ${fontSizes.base};
+          top: 0;
+        }
+      }
+      @media (min-width: ${breakpoints.xl}) {
+        footer {
+          padding: ${measurements.baseUnit * 5}em 0;
+        }
+        ul {
+          margin-bottom: ${measurements.baseUnit * 4}em;
+        }
       }
     `}</style>
   </footer>
