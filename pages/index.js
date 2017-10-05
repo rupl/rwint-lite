10ea75fc25afb53a59c568b264a0ae0aa0795da8
Amy Benson <avbenson@gmail.com>
2017-10-05 16:18:57 +0100

    Disable sw on staging

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export class Index extends React.Component {
 
   componentDidMount () {
     const dev = process.env.NODE_ENV !== 'production'
-    // const staging = window.location && window.location.origin && window.location.origin.indexOf('dev.m.rwdev.org') !== -1
-    const staging = false
+    const staging = window.location && window.location.origin && window.location.origin.indexOf('dev.m.rwdev.org') !== -1
     if (!dev && !staging && 'serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js').catch(err => console.error('Service worker registration failed', err))
     }
