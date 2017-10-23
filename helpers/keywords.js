4ff5da5dde8538cdea25750f7f257441c641df76
Amy Benson <avbenson@gmail.com>
2017-10-23 12:22:30 +0100

    Add vulnerable groups to news keywords

--- helpers/keywords.js
+++ helpers/keywords.js
@@ -1,6 +1,6 @@
 const buildKeywords = (fields) => {
   let keywords = ['humanitarian']
-  const types = ['country', 'source', 'theme', 'disaster', 'disaster_type', 'format', 'language']
+  const types = ['country', 'source', 'theme', 'disaster', 'disaster_type', 'format', 'language', 'vulnerable_groups']
   types.map((type) => {
     if (fields[type]) fields[type].map((item) => { keywords.push(item.name) })
   })
