af265b48490248cf9276b9346a4055e581c0ef46
Amy Benson <avbenson@gmail.com>
2018-01-25 17:10:59 +0000

    Use es5 version of fetch-mock

--- __tests__/services/requests.tests.js
+++ __tests__/services/requests.tests.js
@@ -4,7 +4,7 @@ import { requestCountry, requestCountries, requestDisaster, requestDisasters, re
 import { mockCountry, mockCountries, mockDisaster, mockDisasters, mockEndpoints, mockHeadlines, mockJob, mockJobs, mockTraining,
  mockTrainings, mockReports, mockUpdate } from '../../__fixtures__/data.fixture'
 jest.mock('../../helpers/shuffleArray')
-const fetchMock = require('fetch-mock')
+const fetchMock = require('fetch-mock/es5/server')
 
 describe('API requests', () => {
   let expectedBody, result
